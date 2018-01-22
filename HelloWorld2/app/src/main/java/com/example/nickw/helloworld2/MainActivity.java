218b26a993f6a8e02ed3b122aa36533f2ab2f86d
gallenc <cgallenc@opennms.org>
2018-01-22 17:02:44 +0000

    first project

---
+++ HelloWorld2/app/src/main/java/com/example/nickw/helloworld2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.nickw.helloworld2;
+
+import android.widget.TextView;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        TextView tv=new TextView(this);
+        tv.setText("Hello World!");
+        setContentView(tv);
+
+    }
+}
