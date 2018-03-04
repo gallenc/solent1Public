7170cfd1ce51e3958d0a39afc7e4e4bf1df58ef5
Craig Gallen <cgallen@opennms.org>
2018-03-04 15:25:11 +0000

    added AsyncTasks

---
+++ AsyncTasks1/app/src/main/java/com/example/cgallen/asynctasks1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.cgallen.asynctasks1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    // Example onClick() which would be linked to a button press
+    public void onClick(View view) {
+        TestTask task = new TestTask(this);
+        task.execute();
+
+        TestTask2 task2 = new TestTask2(this);
+        task2.execute("http://www.free-map.org.uk/index.php");
+    }
+
+
+}
+
+
