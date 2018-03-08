1033aa58cee47eb03532fdddd0ae4afcc31c0698
gallenc <cgallenc@opennms.org>
2018-03-08 12:50:51 +0000

    finished json task

--- AsyncTasksJson/app/src/main/java/com/example/cgallen/asynctasks1/MainActivity.java
+++ AsyncTasksJson/app/src/main/java/com/example/cgallen/asynctasks1/MainActivity.java
@@ -12,17 +12,27 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import android.app.AlertDialog;
+
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private static final String BASE_URL = "http://www.free-map.org.uk/course/mad/ws/hits.php";
-    private static final String ARTIST_QUERY="artist=";
+    private static final String ARTIST_QUERY = "artist=";
+    private static final String REQUEST_JSON = "&format=json";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +43,18 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater=getMenuInflater();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.async_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.addArtist) {
+        if (item.getItemId() == R.id.addArtist) {
             // react to the menu item being selected...
-            Intent intent = new Intent(this,PostArtistActivity.class);
-            startActivityForResult(intent,0);
+            Intent intent = new Intent(this, PostArtistActivity.class);
+            startActivityForResult(intent, 0);
             // startActivity(intent);
             return true;
         }
@@ -61,10 +71,17 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     class GetSongsTask extends AsyncTask<String, Void, String> {
 
+        AlertDialog.Builder alertDialogBuilder;
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        }
+
         @Override
         public String doInBackground(String... input) {
             String artist = input[0];
-            String queryUrl = BASE_URL+"?"+ARTIST_QUERY+artist;
+            String queryUrl = BASE_URL + "?" + ARTIST_QUERY + artist + REQUEST_JSON;
             HttpURLConnection conn = null;
             try {
                 URL url = new URL(queryUrl);
@@ -92,8 +109,38 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
         @Override
         public void onPostExecute(String result) {
-            TextView songList = (TextView) findViewById(R.id.songList);
-            songList.setText(result);
+
+            try {
+                JSONArray jsonArr = new JSONArray(result);
+
+                TextView songList = (TextView)  findViewById(R.id.songList);
+                StringBuffer text = new StringBuffer();
+
+                for (int i = 0; i < jsonArr.length(); i++) {
+                    JSONObject curObj = jsonArr.getJSONObject(i);
+                    // "song":"The Importance of Being Idle","artist":"Oasis","year":"2005","month":"Nov","chart":"7","ID":"15","quantity":"198"
+                    String song = curObj.getString("song");
+                    String artist = curObj.getString("artist");
+                    String year = curObj.getString("year");
+                    String month = curObj.getString("month");
+                    String chart = curObj.getString("chart");
+                    String ID = curObj.getString("ID");
+                    String quantity = curObj.getString("quantity");
+
+                    text.append("song='" + song + " " +
+                            ", artist='" + artist + " " +
+                            ", year='" + year + " " +
+                            ", month='" + month + " " +
+                            ", chart='" + chart + " " +
+                            ", ID='" + ID + " " +
+                            ", quantity='" + quantity +
+                            "\n-----\n");
+                }
+                songList.setText(text);
+            } catch (JSONException e) {
+                alertDialogBuilder.setMessage(e.toString()).setPositiveButton("OK", null).show();
+            }
+
         }
     }
 
