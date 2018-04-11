a41b78ac5525068b718db3e12bec4ed5841cb6c3
Craig Gallen <cgallen@opennms.org>
2018-04-11 10:17:55 +0100

    poi map

--- HelloMapPoi/app/src/main/java/com/example/cgallen/hellomap/MapOverlayAppActivity.java
+++ HelloMapPoi/app/src/main/java/com/example/cgallen/hellomap/MapOverlayAppActivity.java
@@ -1,7 +1,9 @@
 package com.example.cgallen.hellomap;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
@@ -11,9 +13,18 @@ import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import android.util.Log;
+
 public class MapOverlayAppActivity extends Activity {
+    private static final String LOG_TAG = MapOverlayAppActivity.class.getName();
+
+    private static final String POI_FILE="poi.txt";
 
     MapView mv;
     ItemizedIconOverlay<OverlayItem> items;
@@ -50,14 +61,71 @@ public class MapOverlayAppActivity extends Activity {
 
         items = new ItemizedIconOverlay<OverlayItem>(this, new ArrayList<OverlayItem>(), markerGestureListener);
 
-        OverlayItem fernhurst = new OverlayItem("Fernhurst", "Village in West Sussex", new GeoPoint(51.05, -0.72));
-        OverlayItem blackdown = new OverlayItem("Blackdown", "highest point in West Sussex", new GeoPoint(51.0581, -0.6897));
-        // NOTE is just this.getDrawable() if supporting API 21+ only
-        fernhurst.setMarker(getResources().getDrawable(R.drawable.marker));
+        ArrayList<OverlayItem> itemslist = csvFileToOverlays();
+        items.addItems(itemslist);
 
-        items.addItem(fernhurst);
-        items.addItem(blackdown);
         mv.getOverlays().add(items);
 
     }
+
+
+
+    private ArrayList<OverlayItem> csvFileToOverlays() {
+        ArrayList<OverlayItem> overalyItems= new ArrayList<OverlayItem>();
+
+       // StringBuffer stringBuffer = new StringBuffer();
+        BufferedReader bufferedReader = null;
+        FileReader fileReader = null;
+        try {
+            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + POI_FILE);
+            if (!file.exists()) {
+                popupMessage("file does not exist: " + file.getAbsolutePath());
+            } else {
+                Log.i(LOG_TAG,"loading points of interest from:"+file.getAbsolutePath());
+                fileReader = new FileReader(file);
+                bufferedReader = new BufferedReader(fileReader);
+
+                String line = null;
+                while ((line = bufferedReader.readLine()) != null) {
+                    try {
+                        Log.i(LOG_TAG,"adding point of interest from:"+line);
+                        // csv format The Crown,pub,nice pub,-1.4011,50.9319
+                        String[] components = line.split(",");
+                        if (components.length != 5)
+                            throw new IllegalArgumentException("invalid string " + line +
+                                    " cannot be split into title, snippet, detail ,lat, lon");
+                        String title = components[0];
+                        String type = components[2];
+                        String snippet = components[3];
+                        String latstr = components[4];
+                        String lonstr = components[5];
+                        double lat = Double.parseDouble(latstr);
+                        double lon = Double.parseDouble(lonstr);
+
+                        OverlayItem overlayItem = new OverlayItem(title, snippet, new GeoPoint(lat, lon));
+                        // NOTE is just this.getDrawable() if supporting API 21+ only
+                        //overlayItem.setMarker(getResources().getDrawable(R.drawable.marker));
+                        overalyItems.add(overlayItem);
+                    } catch (Exception e){
+                        Log.e(LOG_TAG,"problem parsing line:"+line,e);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            popupMessage(e.getMessage());
+        } finally {
+            try {
+                if (bufferedReader != null) bufferedReader.close();
+                if (fileReader != null) fileReader.close();
+            } catch (IOException ex) {
+            }
+        }
+
+        return overalyItems;
+    }
+
+    private void popupMessage(String message) {
+        new AlertDialog.Builder(this).setPositiveButton("OK", null).setMessage(message).show();
+    }
+
 }
